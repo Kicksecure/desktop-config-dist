003526a6c33bec48128bef0a5c89714038f5f92e
Patrick Schleizer <adrelanos@riseup.net>
2019-12-16 06:41:27 -0500

    comment

--- usr/share/livecheck/livecheck.sh
+++ usr/share/livecheck/livecheck.sh
@@ -8,6 +8,9 @@ set -e
 
 if grep -qs "boot=live" /proc/cmdline; then
    ## Live mode is enabled.
+   ## Using sudo because hide-hardware-info.service makes this only readable by
+   ## root, not user.
+   ## https://forums.whonix.org/t/restrict-hardware-information-to-root-testers-wanted/8618/13
    if [ -n "$(sudo --non-interactive /bin/lsblk /dev/disk/by-uuid/26ada0c0-1165-4098-884d-aafd2220c2c6 -o RO | grep "1")" ]; then
       # disk is set to RO, too
       echo "<img>/usr/share/icons/gnome-colors-common/16x16/actions/dialog-apply.png</img>"
