a33b01ae08d0c1910b2207c108fbb6b1a351a496
Patrick Schleizer <adrelanos@riseup.net>
2019-12-16 06:40:28 -0500

    comments

--- usr/share/livecheck/livecheck.sh
+++ usr/share/livecheck/livecheck.sh
@@ -7,7 +7,7 @@
 set -e
 
 if grep -qs "boot=live" /proc/cmdline; then
-   # live mode is enabled
+   ## Live mode is enabled.
    if [ -n "$(sudo --non-interactive /bin/lsblk /dev/disk/by-uuid/26ada0c0-1165-4098-884d-aafd2220c2c6 -o RO | grep "1")" ]; then
       # disk is set to RO, too
       echo "<img>/usr/share/icons/gnome-colors-common/16x16/actions/dialog-apply.png</img>"
@@ -16,7 +16,7 @@ if grep -qs "boot=live" /proc/cmdline; then
       echo "<click>x-www-browser https://whonix.org/wiki/Whonix_Live</click>"
       echo "<txtclick>x-www-browser https://whonix.org/wiki/Whonix_Live</txtclick>"
    else
-      # disk is not set to RO
+      ## Disk is not set to RO.
       echo "<img>/usr/share/icons/Adwaita/16x16/status/dialog-warning.png</img>"
       echo "<txt>Live</txt>"
       echo "<tool>Live mode is enabled but it is still possible to write to the disk. Please power off the machine and set the disk to read-only. See: https://whonix.org/wiki/Whonix_Live or click on the icon for more information.</tool>"
@@ -24,7 +24,7 @@ if grep -qs "boot=live" /proc/cmdline; then
       echo "<txtclick>x-www-browser https://whonix.org/wiki/Whonix_Live</txtclick>"
    fi
 else
-   # live mode is disabled
+   ## Live mode is disabled.
    echo "<img>/usr/share/icons/gnome-colors-common/22x22/status/gtk-info.png</img>"
    echo "<tool>You are using persistent mode. All changes to the disk will be preserved after a reboot. For using live mode, see: https://whonix.org/wiki/Whonix_Live or click on the icon for more information.</tool>"
    echo "<click>x-www-browser https://whonix.org/wiki/Whonix_Live</click>"
