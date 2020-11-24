b0597a0eb0278f98a1fb149af0fbf7b32ee5e2a6
Patrick Schleizer <adrelanos@riseup.net>
2020-11-24 11:16:06 -0500

    refactoring

--- usr/share/livecheck/livecheck.sh
+++ usr/share/livecheck/livecheck.sh
@@ -64,17 +64,9 @@ if ! lsblk_output="$(sudo --non-interactive /bin/lsblk --noheadings --all --raw 
    echo "<img>/usr/share/icons/gnome-colors-common/16x16/status/dialog-error.png</img>"
    ## Show "Error" next to info symbol in systray.
    echo "<txt>Error</txt>"
-   if test -f /usr/share/anon-gw-base-files/gateway || test -f /usr/share/anon-ws-base-files/workstation ; then
-      ## case: Whonix VM
-      echo "<tool>Do not panic. Live mode detection failed. Could not determine if booted into live mode or persistent mode. Please report this bug. See: https://www.whonix.org/wiki/Reporting_Bugs or click on the icon for more information.</tool>"
-      echo "<click>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</click>"
-      echo "<txtclick>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</txtclick>"
-   else
-      ## case: Debian hosts, Kicksecure hosts, non-Whonix hosts, Whonix-Host
-      echo "<tool>Do not panic. Live mode detection failed. Could not determine if booted into live mode or persistent mode. Please report this bug. See: https://www.whonix.org/wiki/Reporting_Bugs or click on the icon for more information.</tool>"
-      echo "<click>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</click>"
-      echo "<txtclick>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</txtclick>"
-   fi
+   echo "<tool>Do not panic. Live mode detection failed. Could not determine if booted into live mode or persistent mode. Please report this bug. See: https://www.whonix.org/wiki/Reporting_Bugs or click on the icon for more information.</tool>"
+   echo "<click>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</click>"
+   echo "<txtclick>x-www-browser https://www.whonix.org/wiki/Reporting_Bugs</txtclick>"
    exit 0
 fi
 ## lsblk exited with exit code 0.
