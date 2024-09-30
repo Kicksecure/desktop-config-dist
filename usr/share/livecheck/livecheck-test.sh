fcaaa7c4759e5b5a5218d8907ccb2b59a4b9ef30
Patrick Schleizer <adrelanos@whonix.org>
2024-09-30 05:12:26 -0400

    improve livecheck tests

--- usr/share/livecheck/livecheck-test.sh
+++ usr/share/livecheck/livecheck-test.sh
@@ -14,24 +14,26 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$MYDIR"
 
+mkdir --parents ~/livechecktest
+
 run_test_case() {
    ./livecheck.sh "$@"
 }
 
 ## Case 1: Booted in grub-live mode, with read-write devices
-run_test_case "boot=live" "1\n1\n0\n0"
+run_test_case "test" "boot=live" "1\n1\n0\n0" > ~/livechecktest/grub-live_read-write.txt
 
 ## Case 2: Booted in ISO Live mode, with read-write devices
-run_test_case "root=live" "1\n1\n0\n0"
+run_test_case "test" "root=live" "1\n1\n0\n0" > ~/livechecktest/iso-live_read-write.txt
 
 ## Case 3: Booted in persistent mode (not live), with read-write devices
-run_test_case "" "1\n1\n0\n0"
+run_test_case "test" "" "1\n1\n0\n0" > ~/livechecktest/persistent_read-write.txt
 
 ## Case 4: Booted in grub-live mode, with read-only devices
-run_test_case "boot=live" "1\n1\n1\n1"
+run_test_case "test" "boot=live" "1\n1\n1\n1" > ~/livechecktest/grub-live_read-only.txt
 
 ## Case 5: Booted in ISO Live mode, with read-only devices
-run_test_case "root=live" "1\n1\n1\n1"
+run_test_case "test" "root=live" "1\n1\n1\n1" > ~/livechecktest/iso-live_read-only.txt
 
 ## Case 6: Booted in persistent mode (not live), with read-only devices
-run_test_case "" "1\n1\n1\n1"
+run_test_case "test" "" "1\n1\n1\n1" > ~/livechecktest/persistent_read-only.txt
